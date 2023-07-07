150449d91e042f49640864c4466261c7e404281c
KuhaiAnna <kuhai.anna@gmail.com>
2023-07-07 23:41:42 +0300

    Remove comments

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,8 +1,7 @@
 const contacts = require("../models/contacts");
 const { HttpError, ctrlWrapper } = require("../helpers");
-const { addSchema, updateSchema } = require("../utils");
 
-const getAll = async (req, res, next) => {
+const getAll = async (res) => {
   const result = await contacts.listContacts();
   res.json(result);
 };
@@ -18,14 +17,6 @@ const getContactById = async (req, res, next) => {
 };
 
 const addContact = async (req, res, next) => {
-  const { error } = addSchema.validate(req.body);
-
-  // ---------
-  //  Якщо в body немає якихось обов'язкових полів, повертає json з ключем {"message": "missing required name field"} і статусом 400
-  if (error) {
-    return next(new HttpError(400, "Missing required field"));
-  }
-
   const result = await contacts.addContact(req.body);
   res.status(201).json(result);
 };
@@ -41,22 +32,7 @@ const removeContact = async (req, res, next) => {
 };
 
 const updateContact = async (req, res, next) => {
-  // Отримує body в json-форматі c оновленням будь-яких полів name, email и phone
-  // Якщо body немає, повертає json з ключем { "message": "missing fields" } і статусом 400
-  const fields = Object.keys(req.body);
-  if (fields.length === 0) {
-    return next(new HttpError(400, "Missing fields"));
-  }
-
-  // -------
-  const { error } = updateSchema.validate(req.body);
-  if (error) {
-    return next(new HttpError(400, "Missing fields"));
-  }
-
-  // Отримує параметр id
   const { contactId } = req.params;
-  // Якщо з body всі добре, викликає функцію updateContact(contactId, body).
   const result = await contacts.updateContact(contactId, req.body);
 
   if (!result) {
