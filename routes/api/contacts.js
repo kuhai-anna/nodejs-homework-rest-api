ed380afcd509ac39c936ea901990cb3801955735
KuhaiAnna <kuhai.anna@gmail.com>
2023-07-07 23:40:54 +0300

    Add decorators

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,15 +2,17 @@ const express = require("express");
 
 const router = express.Router();
 const ctrl = require("../../controllers/contacts");
+const { addSchema, updateSchema } = require("../../schemas/contacts");
+const { validateBody } = require("../../middlewares");
 
 router.get("/", ctrl.getAll);
 
 router.get("/:contactId", ctrl.getContactById);
 
-router.post("/", ctrl.addContact);
+router.post("/", validateBody(addSchema), ctrl.addContact);
 
 router.delete("/:contactId", ctrl.removeContact);
 
-router.put("/:contactId", ctrl.updateContact);
+router.put("/:contactId", validateBody(updateSchema), ctrl.updateContact);
 
 module.exports = router;
