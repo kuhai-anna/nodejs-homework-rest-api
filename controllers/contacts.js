e56b2aae7bebd924a732fdfb126255d803eddb45
KuhaiAnna <kuhai.anna@gmail.com>
2023-07-15 00:44:20 +0300

    Add Mongoose-methods

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,4 +1,4 @@
-const Contact = require("../models/contact");
+const { Contact } = require("../models/contact");
 const { HttpError, ctrlWrapper } = require("../helpers");
 
 const getAll = async (req, res) => {
@@ -6,45 +6,60 @@ const getAll = async (req, res) => {
   res.json(result);
 };
 
-// const getContactById = async (req, res, next) => {
-//   const { contactId } = req.params;
-//   const result = await contacts.getContactById(contactId);
+const getContactById = async (req, res, next) => {
+  const { contactId } = req.params;
+  const result = await Contact.findById(contactId);
 
-//   if (!result) {
-//     return next(new HttpError(404));
-//   }
-//   res.json(result);
-// };
+  if (!result) {
+    return next(new HttpError(404));
+  }
+  res.json(result);
+};
 
-// const addContact = async (req, res, next) => {
-//   const result = await contacts.addContact(req.body);
-//   res.status(201).json(result);
-// };
+const addContact = async (req, res, next) => {
+  const result = await Contact.create(req.body);
+  res.status(201).json(result);
+};
 
-// const removeContact = async (req, res, next) => {
-//   const { contactId } = req.params;
-//   const result = await contacts.removeContact(contactId);
+const removeContact = async (req, res, next) => {
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndRemove(contactId);
 
-//   if (!result) {
-//     return next(new HttpError(404));
-//   }
-//   res.json({ message: "Contact deleted" });
-// };
+  if (!result) {
+    return next(new HttpError(404));
+  }
+  res.json({ message: "Contact deleted" });
+};
 
-// const updateContact = async (req, res, next) => {
-//   const { contactId } = req.params;
-//   const result = await contacts.updateContact(contactId, req.body);
+const updateContact = async (req, res, next) => {
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
 
-//   if (!result) {
-//     return next(new HttpError(404));
-//   }
-//   res.json(result);
-// };
+  if (!result) {
+    return next(new HttpError(404));
+  }
+  res.json(result);
+};
+
+const updateStatusContact = async (req, res, next) => {
+  const { contactId } = req.params;
+  const result = await Contact.findByIdAndUpdate(contactId, req.body, {
+    new: true,
+  });
+
+  if (!result) {
+    return next(new HttpError(404));
+  }
+  res.json(result);
+};
 
 module.exports = {
   getAll: ctrlWrapper(getAll),
-  // getContactById: ctrlWrapper(getContactById),
-  // addContact: ctrlWrapper(addContact),
-  // removeContact: ctrlWrapper(removeContact),
-  // updateContact: ctrlWrapper(updateContact),
+  getContactById: ctrlWrapper(getContactById),
+  addContact: ctrlWrapper(addContact),
+  removeContact: ctrlWrapper(removeContact),
+  updateContact: ctrlWrapper(updateContact),
+  updateStatusContact: ctrlWrapper(updateStatusContact),
 };
