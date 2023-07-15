0a4ca3c3082f627581fb9c84845e784cdccef2a9
KuhaiAnna <kuhai.anna@gmail.com>
2023-07-15 14:32:17 +0300

    Remove Joi schemas from models

--- models/contact.js
+++ models/contact.js
@@ -1,9 +1,6 @@
 const { Schema, model } = require("mongoose");
-const Joi = require("joi");
 const { handleMongooseError } = require("../helpers");
-
-const phoneRegexp =
-  /^((\+?(38)[- ]?)|((38)?[- ]?))(\(0\d{2}\)|(0\d{2}))[- ]?\d{3}[- ]?\d{2}[- ]?\d{2}$/;
+const phoneRegexp = require("../utils/phoneRegexp");
 
 const contactSchema = new Schema(
   {
@@ -28,44 +25,8 @@ const contactSchema = new Schema(
   { versionKey: false, timestamps: true }
 );
 
-const addSchema = Joi.object({
-  name: Joi.string().min(2).max(30).required().messages({
-    "any.required": "Field 'name' is missing",
-  }),
-  email: Joi.string().email().required().messages({
-    "any.required": "Field 'email' is missing",
-  }),
-  phone: Joi.string().required().pattern(phoneRegexp).messages({
-    "any.required": "Field 'phone' is missing",
-    "string.pattern.base":
-      "Please enter a valid phone number. It must consist 10 digits and can begin with the code of Ukraine +380.",
-  }),
-  favorite: Joi.boolean(),
-});
-
-const updateSchema = Joi.object({
-  name: Joi.string().min(2).max(30),
-  email: Joi.string().email(),
-  phone: Joi.string().pattern(phoneRegexp).messages({
-    "string.pattern.base":
-      "Please enter a valid phone number. It must consist 10 digits and can begin with the code of Ukraine +380.",
-  }),
-})
-  .min(1)
-  .messages({
-    "object.min": "Missing fields",
-  });
-
-const updateFavoriteSchema = Joi.object({
-  favorite: Joi.boolean().required().messages({
-    "any.required": "Missing field favorite",
-  }),
-});
-
 contactSchema.post("save", handleMongooseError);
 
 const Contact = model("contact", contactSchema);
 
-const schemas = { addSchema, updateSchema, updateFavoriteSchema };
-
-module.exports = { Contact, schemas };
+module.exports = { Contact };
