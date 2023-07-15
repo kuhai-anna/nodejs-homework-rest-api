d9710295e2593f062f1673cd7ecb0cd3e063ce86
KuhaiAnna <kuhai.anna@gmail.com>
2023-07-15 14:30:46 +0300

    Create Joi schemas

---
+++ schemas/contacts.js
@@ -0,0 +1,38 @@
+const Joi = require("joi");
+const phoneRegexp = require("../utils/phoneRegexp");
+
+const addSchema = Joi.object({
+  name: Joi.string().min(2).max(30).required().messages({
+    "any.required": "Field 'name' is missing",
+  }),
+  email: Joi.string().email().required().messages({
+    "any.required": "Field 'email' is missing",
+  }),
+  phone: Joi.string().required().pattern(phoneRegexp).messages({
+    "any.required": "Field 'phone' is missing",
+    "string.pattern.base":
+      "Please enter a valid phone number. It must consist 10 digits and can begin with the code of Ukraine +380.",
+  }),
+  favorite: Joi.boolean(),
+});
+
+const updateSchema = Joi.object({
+  name: Joi.string().min(2).max(30),
+  email: Joi.string().email(),
+  phone: Joi.string().pattern(phoneRegexp).messages({
+    "string.pattern.base":
+      "Please enter a valid phone number. It must consist 10 digits and can begin with the code of Ukraine +380.",
+  }),
+})
+  .min(1)
+  .messages({
+    "object.min": "Missing fields",
+  });
+
+const updateFavoriteSchema = Joi.object({
+  favorite: Joi.boolean().required().messages({
+    "any.required": "Missing field favorite",
+  }),
+});
+
+module.exports = { addSchema, updateSchema, updateFavoriteSchema };
