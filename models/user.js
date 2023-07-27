24472de535f6b0d882e3acb08e50af6da23a8967
KuhaiAnna <kuhai.anna@gmail.com>
2023-07-28 00:55:23 +0300

    Create the user model

---
+++ models/user.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require("mongoose");
+const { handleMongooseError } = require("../helpers");
+const emailRegexp = require("../utils/emailRegexp");
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Set name for contact"],
+    },
+    email: {
+      type: String,
+      match: emailRegexp,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Set password for user"],
+      minlength: 6,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: String,
+  },
+  { versionKey: false, timestamps: true }
+);
+
+userSchema.post("save", handleMongooseError);
+
+const User = model("user", userSchema);
+
+module.exports = { User };
